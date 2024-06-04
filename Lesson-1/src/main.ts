1b55cee32962d63b689527e15ff75a3f48d8266f
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-05 00:49:21 +0300

    Chore: Finish lesson 1 - Introduction

--- Lesson-1/src/main.ts
+++ Lesson-1/src/main.ts
@@ -1,2 +1,10 @@
-let username = "Wakabi Brian";
+// ==================== Introduction
+let username: string = "Wakabi Brian";
 console.log(username);
+
+let a: number = 12;
+let b: number = 6;
+let c: number = 2;
+
+console.log(a / b);
+console.log(a * b);
