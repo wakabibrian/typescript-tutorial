4b718ab013c53d25eba6c5078a79232d72fb581d
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-05 23:13:53 +0300

    feat: Add arrays with typescript

--- Lesson-3/build/js/main.js
+++ Lesson-3/build/js/main.js
@@ -1,10 +1,20 @@
 "use strict";
 // ==================== Typescript Objects, Arrays, Tuples & Enums
 // Arrays
-let stringArr = ["One", "Hey", "Wakabi"];
-let guitars = ["Strat", "Les Paul", 5150];
-let mixedData = ["EVH", 1984, true];
+let stringArr = ["One", "Hey", "Wakabi"]; // string only
+let guitars = ["Strat", "Les Paul", 5150]; // string and number
+let mixedData = ["EVH", 1984, true]; // string, number, boolean
 // stringArr[0] = 54 //Typescript throws an error
 stringArr[0] = "John";
-stringArr.push(42);
-/;
+// stringArr.push(42); //Typescript throws an error
+stringArr.push("Hey");
+guitars[0] = 1984;
+guitars.unshift("Jim");
+// guitars.unshift(true); //Typescript throws an error
+// stringArr = guitars //Typescript throws an error
+guitars = stringArr;
+mixedData = guitars;
+// guitars = mixedData //Typescript throws an error
+let test = []; // any type
+let bands = [];
+bands.push("Van Halen");
--- Lesson-3/src/main.ts
+++ Lesson-3/src/main.ts
@@ -1,11 +1,25 @@
 // ==================== Typescript Objects, Arrays, Tuples & Enums
 // Arrays
-let stringArr = ["One", "Hey", "Wakabi"];
+let stringArr = ["One", "Hey", "Wakabi"]; // string only
 
-let guitars = ["Strat", "Les Paul", 5150];
+let guitars = ["Strat", "Les Paul", 5150]; // string and number
 
-let mixedData = ["EVH", 1984, true];
+let mixedData = ["EVH", 1984, true]; // string, number, boolean
 
 // stringArr[0] = 54 //Typescript throws an error
 stringArr[0] = "John";
-stringArr.push(42); /
+// stringArr.push(42); //Typescript throws an error
+stringArr.push("Hey");
+
+guitars[0] = 1984;
+guitars.unshift("Jim");
+// guitars.unshift(true); //Typescript throws an error
+
+// stringArr = guitars //Typescript throws an error
+guitars = stringArr;
+mixedData = guitars;
+// guitars = mixedData //Typescript throws an error
+
+let test = []; // any type
+let bands: string[] = [];
+bands.push("Van Halen");
