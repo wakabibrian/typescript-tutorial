7099c717f525087fe0033902b3a89c52d7a5e600
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-07 20:08:03 +0300

    fix: Add enum feature

--- Lesson-3/build/js/main.js
+++ Lesson-3/build/js/main.js
@@ -75,3 +75,13 @@ const greetGuitarist2 = (guitarist) => {
     }
     return "Hello!";
 };
+// Enums
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.U);
