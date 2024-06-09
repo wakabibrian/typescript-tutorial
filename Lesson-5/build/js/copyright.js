28807eb6443be691c5822281eb342f408b69d472
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-09 06:11:17 +0300

    feat: Add exercise and solution

---
+++ Lesson-5/build/js/copyright.js
@@ -0,0 +1,21 @@
+"use strict";
+// Original JS code
+// const year = document.getElementById("year");
+// const thisYear = new Date().getFullYear();
+// year.setAttribute("datetime", thisYear);
+// year.textContent = thisYear;
+// 1st solution
+// let year: HTMLElement | null;
+// year = document.getElementById("year");
+// let thisYear: string;
+// thisYear = new Date().getFullYear().toString();
+// if (year) {
+//   year.setAttribute("datetime", thisYear);
+//   year.textContent = thisYear;
+// }
+// 2nd solution
+// const year = <HTMLSpanElement>document.getElementById("year");
+const year = document.getElementById("year");
+const thisYear = new Date().getFullYear().toString();
+year.setAttribute("datetime", thisYear);
+year.textContent = thisYear;
