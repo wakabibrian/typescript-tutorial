f136722e44abf59f11548d36aaec811979de9d23
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-09 22:06:48 +0300

    feat: Add visibility modifiers

--- Lesson-6/build/js/main.js
+++ Lesson-6/build/js/main.js
@@ -1,8 +1,25 @@
 "use strict";
 // ==================== Typescript Classes
 // Basic class
+// class Coder {
+//   name: string;
+//   music: string;
+//   age: number;
+//   lang: string;
+//   constructor(name: string, music: string, age: number, lang: string) {
+//     this.name = name;
+//     this.music = music;
+//     this.age = age;
+//     this.lang = lang;
+//   }
+// }
+// Visibility modifier - Less repetitive
 class Coder {
     constructor(name, music, age, lang) {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
         this.name = name;
         this.music = music;
         this.age = age;
--- Lesson-6/src/main.ts
+++ Lesson-6/src/main.ts
@@ -1,12 +1,27 @@
 // ==================== Typescript Classes
 // Basic class
-class Coder {
-  name: string;
-  music: string;
-  age: number;
-  lang: string;
+// class Coder {
+//   name: string;
+//   music: string;
+//   age: number;
+//   lang: string;
 
-  constructor(name: string, music: string, age: number, lang: string) {
+//   constructor(name: string, music: string, age: number, lang: string) {
+//     this.name = name;
+//     this.music = music;
+//     this.age = age;
+//     this.lang = lang;
+//   }
+// }
+
+// Visibility modifier - Less repetitive
+class Coder {
+  constructor(
+    public readonly name: string,
+    readonly music: string,
+    private age: number,
+    protected lang: string
+  ) {
     this.name = name;
     this.music = music;
     this.age = age;
