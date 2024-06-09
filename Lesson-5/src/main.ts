4b8de91fdc98a01aaa3cbea477559578e82dce2a
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-09 04:23:03 +0300

    feat: Convert to more or less specific type

--- Lesson-5/src/main.ts
+++ Lesson-5/src/main.ts
@@ -1,126 +1,13 @@
-// ==================== Typescript Functions
+// ==================== Type assertions and Type casting (Mean the same)
+// Sometimes you will have information about the type of a value that TypeScript can’t know about.
+type One = string;
+type Two = string | number;
+type Three = "hello";
 
-// Type Aliases - Representing our typescript type with a different name
-type stringOrNumber = string | number;
-
-type stringOrNumberArray = (string | number)[];
-
-type Guitarist = {
-  name?: string;
-  active: boolean;
-  albums: stringOrNumberArray;
-};
-
-type userId = stringOrNumber;
-
-// interface postId = typeOrNumber; //This can't work
-
-// Literal types
-let myName: "Wakabi";
-// myName = "Brian" // Throws a typescript error
-myName = "Wakabi";
-
-let username: "Wakabi" | "Brian" | "Miracle";
-username = "Miracle";
-
-// Functions
-const add = (a: number, b: number): number => {
-  return a + b;
-};
-
-// Function with no return value
-const logMsg = (message: any): void => {
-  console.log(message);
-};
-
-logMsg("Hello!");
-logMsg(add(2, 3));
-// logMsg(add("a", 3)); // Throws a typescript error
-
-// The function keyword
-let subtract = function (c: number, d: number): number {
-  return c - d;
-};
-
-// Function Type Aliases
-type mathFunction = (a: number, b: number) => number;
-
-let multiply: mathFunction = function (c, d) {
-  return c * d;
-};
-
-logMsg(multiply(2, 2));
-
-// Function Interface
-interface mathFunction2 {
-  (a: number, b: number): number;
-}
-
-let multiply2: mathFunction = function (c, d) {
-  return c * d;
-};
-
-logMsg(multiply2(2, 2));
-
-// Optional parameters
-const addAll = (a: number, b: number, c?: number): number => {
-  if (typeof c !== "undefined") {
-    return a + b + c;
-  }
-  return a + b;
-};
-
-// Default parameters
-const sumAll = (a: number, b: number, c: number = 2): number => {
-  return a + b + c;
-};
-
-logMsg(addAll(2, 3, 2));
-logMsg(addAll(2, 3));
-logMsg(sumAll(2, 3, 4));
-logMsg(sumAll(2, 3));
-
-const sumAll2 = (a: number = 10, b: number, c: number = 2): number => {
-  return a + b + c;
-};
-
-logMsg(sumAll2(undefined, 3));
-
-// Rest parameters
-const total = (a: number, ...nums: number[]): number => {
-  return a + nums.reduce((prev, curr) => prev + curr);
-};
-
-logMsg(total(10, 2, 3));
-
-// The never type - Functions that throw errors and infinite/endless loops
-const createError = (errMsg: string): never => {
-  throw new Error(errMsg);
-};
-
-const infinite = () => {
-  let i: number = 1;
-
-  while (true) {
-    i++;
-    if (i > 100) break; //If tis wasn't there it would be a never type
-  }
-};
-
-// use of the never type
-const numberOrString = (value: number | string): string => {
-  if (typeof value === "string") return "string";
-  if (typeof value === "number") return "number";
-  return createError("This should never happen!");
-};
-
-// Custom type guard
-const isNumber = (value: any): boolean => {
-  return typeof value === "number" ? true : false;
-};
-
-const numberOrString2 = (value: number | string): string => {
-  if (typeof value === "string") return "string";
-  if (isNumber(value)) return "number";
-  return createError("This should never happen!");
-};
+// Converting Types with assertions
+// Convert to more or less specific
+let a: One = "hello";
+let b = a as Two; // Converted to a Less specific type (number or string);
+b = "Wakabi";
+b = 2;
+let c = a as Three; // Converted to a more specific type ("hello")
