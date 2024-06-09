9b37042a624e32a3da03c670d161a6e3f548f557
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-09 21:47:10 +0300

    feat: Add larger class

--- Lesson-6/build/js/main.js
+++ Lesson-6/build/js/main.js
@@ -2,7 +2,10 @@
 // ==================== Typescript Classes
 // Basic class
 class Coder {
-    constructor(name) {
+    constructor(name, music, age, lang) {
         this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
     }
 }
--- Lesson-6/src/main.ts
+++ Lesson-6/src/main.ts
@@ -2,8 +2,14 @@
 // Basic class
 class Coder {
   name: string;
+  music: string;
+  age: number;
+  lang: string;
 
-  constructor(name: string) {
+  constructor(name: string, music: string, age: number, lang: string) {
     this.name = name;
+    this.music = music;
+    this.age = age;
+    this.lang = lang;
   }
 }
