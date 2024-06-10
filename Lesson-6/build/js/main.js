82ab6adf96ddf59dd4ba414d894faabd70565764
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-10 23:28:11 +0300

    feat: Add Getters and Setters

--- Lesson-6/build/js/main.js
+++ Lesson-6/build/js/main.js
@@ -79,3 +79,26 @@ console.log(Amy.id);
 console.log(Steve.id);
 console.log(John.id);
 console.log(Peeps.getCount());
+// Getters and Setters
+class Bands {
+    constructor() {
+        this.dataState = [];
+    }
+    get data() {
+        return this.dataState;
+    }
+    set data(value) {
+        if (Array.isArray(value) && value.every((el) => typeof el === "string")) {
+            this.dataState = value;
+            return;
+        }
+        else
+            throw new Error("Param is not an array of strings");
+    }
+}
+const MyBands = new Bands();
+MyBands.data = ["Neil Young", "Led Zep"];
+console.log(MyBands.data);
+MyBands.data = [...MyBands.data, "Zz Top"];
+console.log(MyBands.data);
+// MyBands.data = [3, 4]; //Throws a typescript error
