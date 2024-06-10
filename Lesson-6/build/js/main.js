c840868224c99f9cd91d2f8bbc8f638acf8c35ea
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-10 07:09:37 +0300

    feat: Add Private and Protected examples

--- Lesson-6/build/js/main.js
+++ Lesson-6/build/js/main.js
@@ -15,7 +15,7 @@
 // }
 // Visibility modifier - Less repetitive
 class Coder {
-    constructor(name, music, age, lang) {
+    constructor(name, music, age, lang = "Typescript") {
         this.name = name;
         this.music = music;
         this.age = age;
@@ -25,4 +25,14 @@ class Coder {
         this.age = age;
         this.lang = lang;
     }
+    getAge() {
+        return `Hello I'm ${this.age}`;
+    }
 }
+// Private and Protected examples
+// Protected can be accessed inside of the class and derived/sub classes (extended to sub classes)
+// Private can only be accessed inside of that class
+const Wakabi = new Coder("Wakabi", "Rock", 34);
+console.log(Wakabi.getAge());
+console.log(Wakabi.age); //Throws an error because it is private (can only be accessed within that class)
+console.log(Wakabi.lang); //Throws an error because it is protected (can only be accessed within that class and subclasses)
--- Lesson-6/src/main.ts
+++ Lesson-6/src/main.ts
@@ -22,11 +22,23 @@ class Coder {
     public readonly name: string,
     readonly music: string,
     private age: number,
-    protected lang: string
+    protected lang: string = "Typescript"
   ) {
     this.name = name;
     this.music = music;
     this.age = age;
     this.lang = lang;
   }
+
+  public getAge() {
+    return `Hello I'm ${this.age}`;
+  }
 }
+
+// Private and Protected examples
+// Protected can be accessed inside of the class and derived/sub classes (extended to sub classes)
+// Private can only be accessed inside of that class
+const Wakabi = new Coder("Wakabi", "Rock", 34);
+console.log(Wakabi.getAge());
+console.log(Wakabi.age); //Throws an error because it is private (can only be accessed within that class)
+console.log(Wakabi.lang); //Throws an error because it is protected (can only be accessed within that class and subclasses)
