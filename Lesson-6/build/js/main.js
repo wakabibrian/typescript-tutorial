c332d5647f80a4fca624b3a71000c3fe6b7cd8c7
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-10 07:28:51 +0300

    feat: Add Extends for classes

--- Lesson-6/build/js/main.js
+++ Lesson-6/build/js/main.js
@@ -36,3 +36,18 @@ const Wakabi = new Coder("Wakabi", "Rock", 34);
 console.log(Wakabi.getAge());
 // console.log(Wakabi.age); //Throws an error because it is private (can only be accessed within that class) but it is legal Javascript
 // console.log(Wakabi.lang); //Throws an error because it is protected (can only be accessed within that class and subclasses) but it is legal Javascript
+// Extends for subclasses
+class WebDev extends Coder {
+    constructor(computer, name, music, age) {
+        super(name, music, age);
+        this.computer = computer;
+        this.computer = computer;
+    }
+    getLang() {
+        return `I write ${this.lang}`; //Lang is default
+    }
+}
+const Britney = new WebDev("Mac", "Britney", "Lofi", 5);
+console.log(Britney.getLang());
+// console.log(Britney.age); //Throws an error because it is private (can only be accessed within class Coder)
+// console.log(Britney.lang); //Throws an error because it is private (can only be accessed within class Coder and it's subclasses)
