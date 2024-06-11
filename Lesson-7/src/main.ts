bbcf371f9238a857b9c15efeba272e3d48175a5f
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-11 04:32:54 +0300

    feat: Add what index signatures are

--- Lesson-7/src/main.ts
+++ Lesson-7/src/main.ts
@@ -1,1 +1,3 @@
 // ==================== Typescript Index Signatures, keyof Assertions & the Record Utility Type
+// Index signatures are useful when you are creating objects but you don't know the exact names of the object keys
+// but you do know the shape of the object and you can declare the type of the keys and the types of the value
