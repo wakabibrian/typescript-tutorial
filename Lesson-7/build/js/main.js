2ddcf93c47bfd83b45a140d87c72888bd87e1863
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-11 04:52:40 +0300

    feat: Add why you need an index signature

--- Lesson-7/build/js/main.js
+++ Lesson-7/build/js/main.js
@@ -2,3 +2,20 @@
 // ==================== Typescript Index Signatures, keyof Assertions & the Record Utility Type
 // Index signatures are useful when you are creating objects but you don't know the exact names of the object keys
 // but you do know the shape of the object and you can declare the type of the keys and the types of the value
+const todaysTransactions = {
+    Pizza: -10,
+    Books: -5,
+    Job: 50,
+};
+console.log(todaysTransactions.Pizza);
+console.log(todaysTransactions["Pizza"]);
+let prop = "Pizza";
+console.log(todaysTransactions[prop]); //Throws a Typescript error - We haven't created an index signature and we are trying to access it dynamically
+const todaysNet = (transactions) => {
+    let total = 0;
+    for (const transact in transactions) {
+        total += transactions[transact]; //Throws a Typescript error - Dynamically accessing the value
+    }
+    return total;
+};
+console.log(todaysNet(todaysTransactions));
--- Lesson-7/src/main.ts
+++ Lesson-7/src/main.ts
@@ -1,3 +1,32 @@
 // ==================== Typescript Index Signatures, keyof Assertions & the Record Utility Type
 // Index signatures are useful when you are creating objects but you don't know the exact names of the object keys
 // but you do know the shape of the object and you can declare the type of the keys and the types of the value
+
+// Why you need an index signature
+interface TransactionObj {
+  Pizza: number;
+  Books: number;
+  Job: number;
+}
+
+const todaysTransactions: TransactionObj = {
+  Pizza: -10,
+  Books: -5,
+  Job: 50,
+};
+
+console.log(todaysTransactions.Pizza);
+console.log(todaysTransactions["Pizza"]);
+
+let prop: string = "Pizza";
+console.log(todaysTransactions[prop]); //Throws a Typescript error - We haven't created an index signature and we are trying to access it dynamically
+
+const todaysNet = (transactions: TransactionObj): number => {
+  let total = 0;
+  for (const transact in transactions) {
+    total += transactions[transact]; //Throws a Typescript error - Dynamically accessing the value
+  }
+  return total;
+};
+
+console.log(todaysNet(todaysTransactions));
