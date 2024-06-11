2705103fe02986b09448a967d3044f7e2b1ffcb2
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-11 22:05:01 +0300

    feat: Add Record utility type vs index signatures

--- Lesson-7/build/js/main.js
+++ Lesson-7/build/js/main.js
@@ -35,3 +35,17 @@ for (const key in student) {
     console.log(`${key}: ${student[key]}`);
 }
 Object.keys(student).map((key) => console.log(student[key]));
+// Record utility type vs index signatures
+const logStudentKey = (student, key) => {
+    console.log(`Student ${key}: ${student[key]}`);
+};
+logStudentKey(student, "GPA");
+logStudentKey(student, "name");
+const monthlyIncomes = {
+    salary: 500,
+    bonus: 100,
+    sidehustle: 250,
+};
+for (const revenue in monthlyIncomes) {
+    console.log(monthlyIncomes[revenue]);
+}
--- Lesson-7/src/main.ts
+++ Lesson-7/src/main.ts
@@ -97,3 +97,28 @@ for (const key in student) {
 Object.keys(student).map((key) =>
   console.log(student[key as keyof typeof student])
 );
+
+// Record utility type vs index signatures
+const logStudentKey = (student: Student, key: keyof Student): void => {
+  console.log(`Student ${key}: ${student[key]}`);
+};
+
+logStudentKey(student, "GPA");
+logStudentKey(student, "name");
+
+// interface Incomes {
+//   [key: string]: number;
+// }
+
+type Streams = "salary" | "bonus" | "sidehustle";
+type Incomes = Record<Streams, number | string>; // Utility type record - smaller than interface
+
+const monthlyIncomes: Incomes = {
+  salary: 500,
+  bonus: 100,
+  sidehustle: 250,
+};
+
+for (const revenue in monthlyIncomes) {
+  console.log(monthlyIncomes[revenue as keyof Incomes]);
+}
