8a899c558a4c3ffe23106fc58790859fb520e713
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-12 07:39:15 +0300

    feat: Add Generics Syntax

--- Lesson-8/build/js/main.js
+++ Lesson-8/build/js/main.js
@@ -2,3 +2,5 @@
 // ==================== Typescript Generics
 // Sometimes we don't know what types will be passed to our functions, interface, type alias etc
 // Generics provides a type variables...
+// Basic Example and syntax
+const echo = (arg) => arg;
--- Lesson-8/src/main.ts
+++ Lesson-8/src/main.ts
@@ -1,3 +1,6 @@
 // ==================== Typescript Generics
 // Sometimes we don't know what types will be passed to our functions, interface, type alias etc
 // Generics provides a type variables...
+
+// Basic Example and syntax
+const echo = <T>(arg: T): T => arg;
