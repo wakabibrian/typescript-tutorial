5c5a8a1820adf0ce07b9c1d5ce8b6ecd1ac447ee
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-12 21:14:14 +0300

    feat: Add isTrue with key of assertion example

--- Lesson-8/src/main.ts
+++ Lesson-8/src/main.ts
@@ -15,3 +15,29 @@ console.log(isObj("John"));
 console.log(isObj([1, 2, 3]));
 console.log(isObj({ name: "John" }));
 console.log(isObj(null));
+
+// isTrue with keyof assertion
+const isTrue = <T>(arg: T): { arg: T; is: boolean } => {
+  if (Array.isArray(arg) && !arg.length) {
+    return { arg, is: false };
+  }
+  if (isObj(arg) && !Object.keys(arg as keyof T).length) {
+    return { arg, is: false };
+  }
+  return { arg, is: !!arg };
+};
+
+console.log(isTrue(false));
+console.log(isTrue(0));
+console.log(isTrue(true));
+console.log(isTrue(1));
+console.log(isTrue("Dave"));
+console.log(isTrue(""));
+console.log(isTrue(null));
+console.log(isTrue(undefined));
+console.log(isTrue({})); // modified
+console.log(isTrue({ name: "Dave" }));
+console.log(isTrue([])); // modified
+console.log(isTrue([1, 2, 3]));
+console.log(isTrue(NaN));
+console.log(isTrue(-0));
