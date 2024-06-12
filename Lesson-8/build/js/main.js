bd1567eade8e40642457973c5546847737719879
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-12 07:33:12 +0300

    feat: Add what Generics are

--- Lesson-8/build/js/main.js
+++ Lesson-8/build/js/main.js
@@ -1,2 +1,4 @@
 "use strict";
 // ==================== Typescript Generics
+// Sometimes we don't know what types will be passed to our functions, interface, type alias etc
+// Generics provides a type variables...
--- Lesson-8/src/main.ts
+++ Lesson-8/src/main.ts
@@ -1,1 +1,3 @@
 // ==================== Typescript Generics
+// Sometimes we don't know what types will be passed to our functions, interface, type alias etc
+// Generics provides a type variables...
