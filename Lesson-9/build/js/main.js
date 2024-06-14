8f3b51a9e5f52ff95948de04cb946956c40e6b5b
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-14 05:14:20 +0300

    feat: Add Required and Readonly utility types

--- Lesson-9/build/js/main.js
+++ Lesson-9/build/js/main.js
@@ -9,3 +9,13 @@ const assign1 = {
 };
 console.log(updateAssignment(assign1, { grade: 95 }));
 const assignGraded = updateAssignment(assign1, { grade: 95 });
+//Required and Readonly
+const recordAssignment = (assign) => {
+    //All required including verified
+    // Send to database
+    return assign;
+};
+const assignVerified = Object.assign(Object.assign({}, assignGraded), { verified: true });
+// assignVerified.grade = 88; //Typescript throws an error because it is readonly
+// recordAssignment(assignGraded); //Typescript throws an error because it is missing verified
+recordAssignment(Object.assign(Object.assign({}, assignGraded), { verified: true })); //Typescript throws an error because it is missing verified
--- Lesson-9/src/main.ts
+++ Lesson-9/src/main.ts
@@ -22,3 +22,20 @@ const assign1: Assignment = {
 
 console.log(updateAssignment(assign1, { grade: 95 }));
 const assignGraded: Assignment = updateAssignment(assign1, { grade: 95 });
+
+//Required and Readonly
+const recordAssignment = (assign: Required<Assignment>): Assignment => {
+  //All required including verified
+  // Send to database
+  return assign;
+};
+
+const assignVerified: Readonly<Assignment> = {
+  //Can not change these values
+  ...assignGraded,
+  verified: true,
+};
+
+// assignVerified.grade = 88; //Typescript throws an error because it is readonly
+// recordAssignment(assignGraded); //Typescript throws an error because it is missing verified
+recordAssignment({ ...assignGraded, verified: true }); //Typescript throws an error because it is missing verified
