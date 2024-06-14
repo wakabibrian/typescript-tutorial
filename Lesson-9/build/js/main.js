9ddf9202b5eb1ad13960b21d57cb41ec64d05e42
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-14 06:06:21 +0300

    feat: Add Awaited utility type

--- Lesson-9/build/js/main.js
+++ Lesson-9/build/js/main.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const updateAssignment = (assign, propsToUpdate) => {
     return Object.assign(Object.assign({}, assign), propsToUpdate);
 };
@@ -53,3 +62,15 @@ console.log(tsAssign);
 const assignArgs = ["Generics", 100];
 const tsAssign2 = createNewAssign(...assignArgs);
 console.log(tsAssign2);
+const fetchUsers = () => __awaiter(void 0, void 0, void 0, function* () {
+    const data = yield fetch("https://jsonplaceholder.typicode.com/users")
+        .then((res) => {
+        return res.json();
+    })
+        .catch((err) => {
+        if (err instanceof Error)
+            console.log(err.message);
+    });
+    return data;
+});
+fetchUsers().then((users) => console.log(users));
--- Lesson-9/src/main.ts
+++ Lesson-9/src/main.ts
@@ -111,3 +111,26 @@ const assignArgs: AssignParams = ["Generics", 100];
 
 const tsAssign2: NewAssign = createNewAssign(...assignArgs);
 console.log(tsAssign2);
+
+// Awaited - helps us with a return type of promise
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+}
+
+const fetchUsers = async (): Promise<User[]> => {
+  const data = await fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => {
+      if (err instanceof Error) console.log(err.message);
+    });
+  return data;
+};
+
+type FetchUsersReturnType = Awaited<ReturnType<typeof fetchUsers>>;
+
+fetchUsers().then((users) => console.log(users));
