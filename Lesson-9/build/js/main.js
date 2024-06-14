97c73a01bd60b58bc8d1a557fed7ea0cc42dc637
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-14 05:02:38 +0300

    feat: Add Partial Utility type

--- Lesson-9/build/js/main.js
+++ Lesson-9/build/js/main.js
@@ -1,2 +1,11 @@
 "use strict";
-// ==================== Typescript Utility Types
+const updateAssignment = (assign, propsToUpdate) => {
+    return Object.assign(Object.assign({}, assign), propsToUpdate);
+};
+const assign1 = {
+    studentId: "compsci123",
+    title: "Final Project",
+    grade: 0,
+};
+console.log(updateAssignment(assign1, { grade: 95 }));
+const assignGraded = updateAssignment(assign1, { grade: 95 });
