fe3e24d2f995401e3f1cd52823f7b9f868d53cf6
Wakabi Brian <wakabibrian95@gmail.com>
2024-06-14 05:40:38 +0300

    feat: Add Exclude and Extract utility types

--- Lesson-9/src/main.ts
+++ Lesson-9/src/main.ts
@@ -81,3 +81,8 @@ const preview: AssignPreview = {
   studentId: "k123",
   title: "Final Project",
 };
+
+// Exclude and Extract
+type adjustedGrade = Exclude<LetterGrades, "U">;
+
+type highGrades = Extract<LetterGrades, "A" | "B">;
